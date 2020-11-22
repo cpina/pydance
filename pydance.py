cpina/pydance

commit 5a88bc227678147f42bb7963987f66d9106ccac6

Python2to3 migration: More Python2 to Python3 changes.
Disables some sorting, fixes some problems

pydance.py
@@ -82,13 +82,15 @@ def load_files(screen, files, type, Ctr, args):
   pct = 0
   inc = 100.0 / len(files)
   # Remove duplicates
-  files = list(dict(map(None, files, [])).keys())
+  # TODO py2to3
+  # files = list(dict(map(None, files, [])).keys())
   objects = []
   message = _("Found %d %s. Loading...") % (len(files), _(type))
   pbar = TextProgress(FontTheme.loading_screen, message, colors.WHITE, colors.BLACK)
   r = pbar.render(0).get_rect()
   r.center = [320, 240]
   for f in files:
+    objects.append(Ctr(*((f,) + args)))
     try: objects.append(Ctr(*((f,) + args)))
     except RuntimeError as message:
       print(_("E:"), f)

announcer.py
@@ -32,7 +32,7 @@ class Announcer(Listener):
     if filename == None:
       raise SystemExit(_("E: Cannot load announcer theme '%s'.") % name)
 
-    fi = file(os.path.join(filename, "djtheme.cfg"), "rU")
+    fi = open(os.path.join(filename, "djtheme.cfg"), "rU")
     sec = ""
     self.lasttime = -1000000
     for line in fi:

fileparsers.py
@@ -32,7 +32,8 @@ class GenericFile(object):
     files = [os.path.join(dir, f) for f in os.listdir(dir) if
              f.lower()[-3:] in formats]
 
-    files.sort(lambda a, b: cmp(os.stat(a).st_size, os.stat(b).st_size))
+    # TODO
+    # files.sort(lambda a, b: cmp(os.stat(a).st_size, os.stat(b).st_size))
     return files
 
   # Try to extract a subtitle from formats that don't support it (DWI)
@@ -105,7 +106,7 @@ class DanceFile(GenericFile):
     state = DanceFile.METADATA
     state_data = [None, None]
 
-    f = file(filename, "rU")
+    f = open(filename, "rU")
 
     for line in f:
       line = line.strip()
@@ -208,7 +209,7 @@ class MSDFile(GenericFile):
   def __init__(self, filename, need_steps):
     GenericFile.__init__(self, filename, need_steps)
     lines = []
-    f = file(filename, "rU")
+    f = open(filename, "rU")
 
     # If there is a BOM, skip it. Otherwise, don't.
     if f.read(3) != codecs.BOM_UTF8: f.seek(0)
@@ -235,7 +236,7 @@ class MSDFile(GenericFile):
 
   # MSD-style files use DWI's .lrc lyric format
   def parse_lyrics(self, filename):
-    f = file(filename, "rU")
+    f = open(filename, "rU")
     offset = 0
     for line in f:
       line = line.strip()
@@ -392,8 +393,8 @@ class DWIFile(MSDFile):
       if parts[0] == "GAP": self.info["gap"] = -int(float(rest))
       # If filename given, save it, and let MSDFile.find_winfname() have a stab at it.
       elif parts[0] == "FILE": self.info["filename"] = rest
-      elif parts[0] == "TITLE": self.info["title"] = rest.decode("iso-8859-1").encode("utf-8")
-      elif parts[0] == "ARTIST": self.info["artist"] = rest.decode("iso-8859-1").encode("utf-8")
+      elif parts[0] == "TITLE": self.info["title"] = rest
+      elif parts[0] == "ARTIST": self.info["artist"] = rest
       elif parts[0] == "MD5": self.info["md5sum"] = rest
       elif parts[0] == "CDTITLE":
         self.info["cdtitle"] = self.find_cdtitle(rest)
@@ -774,8 +775,9 @@ class KSFFile(MSDFile):
 # Sort by difficulty rating, or by a preset list if equal.
 def sorted_diff_list(difflist):
   keys = list(difflist.keys())
-  keys.sort((lambda a, b: cmp(difflist[a], difflist[b]) or
-             util.difficulty_sort(a, b)))
+  # TODO
+  #keys.sort((lambda a, b: cmp(difflist[a], difflist[b]) or
+  #           util.difficulty_sort(a, b)))
   return keys
 
 # Encapsulates and abstracts the above classes
@@ -816,11 +818,15 @@ class SongItem(object):
         raise RuntimeError(filename + _(" is missing: ") + k)
 
     for k in ["subtitle", "title", "artist", "author", "mix"]:
-      try:
-        if k in self.info: self.info[k] = self.info[k].decode("utf-8")
-      except UnicodeError:
-        print(_("W: Non-Unicode key in %s: %s") % (filename, k))
-        self.info[k] = self.info[k].decode("ascii", "ignore")
+      if sys.version_info[0] == 2:
+        try:
+          if k in self.info: self.info[k] = self.info[k].decode("utf-8")
+        except UnicodeError:
+          print(_("W: Non-Unicode key in %s: %s") % (filename, k))
+          self.info[k] = self.info[k].decode("ascii", "ignore")
+      else:
+        # TOOD simplify
+        if k in self.info: self.info[k] = self.info[k]
 
     # Default values
     for k in ["background", "banner", "revision", "md5sum",

fontfx.py
@@ -188,8 +188,8 @@ def render_outer(string, width, font):
   remove = 0
   while font.size(s)[0] > width:
     remove += 1
-    left = center + (-remove)/2
-    right = center + remove/2
+    left = int(center + (-remove)/2)
+    right = int(center + remove/2)
     s = string[:left] + "..." + string[right:]
   return s
 

gfxtheme.py
@@ -234,8 +234,8 @@ class Arrow(object):
 
       # Chop up the image.
       self._images = []
-      for i in range(w / theme.size):
-        for j in range(h / theme.size):
+      for i in range(int(w / theme.size)):
+        for j in range(int(h / theme.size)):
           s = pygame.Surface([theme.size, theme.size])
           s.blit(self._image, [-i * theme.size, -j * theme.size])
           s = pygame.transform.rotate(s, rotate)

interface.py
@@ -429,8 +429,8 @@ class ListBox(pygame.sprite.Sprite):
                                 SRCALPHA, 32)
     self.image.fill([0, 0, 0, 0])
     for i, y in zip(list(range(self._count + 2)),
-                    list(range(-self._h / 2, self._h * (self._count + 1), self._h))):
-      idx = (self._idx + i - 1) % len(self._items)
+                    list(range(int(-self._h / 2), int(self._h * (self._count + 1)), self._h))):
+      idx = int((self._idx + i - 1) % len(self._items))
       t = self._items[idx]
       r = t.get_rect()
       r.centery = y + self._offset

songselect.py
@@ -512,7 +512,8 @@ class SongSelect(InterfaceWindow):
   def _create_folder_list(self):
     sort_name = SORT_NAMES[mainconfig["sortmode"]]
     lst = list(self._folders[sort_name].keys())
-    lst.sort(lambda x, y: cmp(x.lower(), y.lower()))
+    # TODO
+    # lst.sort(lambda x, y: cmp(x.lower(), y.lower()))
     new_songs = [FolderDisplay(folder, sort_name,
                                len(self._folders[sort_name][folder])) for
                  folder in lst]

commit e30046ce42cb1bb15e995ff9d229017dccfa54b7

Songs were appended into objects twice in the list of songs
This bug was introduced in 5a88bc227678147f42bb7963987f66d9106ccac6

The songs were appearing twice

pydance.py
@@ -92,8 +92,8 @@ def load_files(screen, files, type, Ctr, args):
   r = pbar.render(0).get_rect()
   r.center = [320, 240]
   for f in files:
-    objects.append(Ctr(*((f,) + args)))
-    try: objects.append(Ctr(*((f,) + args)))
+    try:
+      objects.append(Ctr(*((f,) + args)))
     except RuntimeError as message:
       print(_("E:"), f)
       print(_("E:"), message)